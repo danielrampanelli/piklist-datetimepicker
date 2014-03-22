38d26504c15e39b4eb281dddc068b4e24e80f475
Daniel Rampanelli <hello@neuralquery.com>
2014-03-22 15:41:33 +0100

    Call already present "js_callback" independent of own initialisation logic.

--- assets/datetimepicker.js
+++ assets/datetimepicker.js
@@ -4,11 +4,11 @@
             if (!$(this).hasClass('hasDateTimePicker')) {
                 $(this).addClass('hasDateTimePicker');
                 $(this).datetimepicker($.extend({}, field.options));
-                
-                if (field.other_js_callback) {
-                    window[field.other_js_callback](field);
-                }
             }
         });
+        
+        if (field.other_js_callback) {
+            window[field.other_js_callback](field);
+        }
     };
 })(jQuery);
