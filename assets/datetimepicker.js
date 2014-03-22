c8f1e8fe5252bb1c7e6dc33d977a98da68402d3f
Daniel Rampanelli <hello@neuralquery.com>
2014-03-22 10:25:06 +0100

    Removed inlined parameters, they are now taken from the "options" defined in the field declaration.

--- assets/datetimepicker.js
+++ assets/datetimepicker.js
@@ -2,10 +2,7 @@
     window.neuralquery_datetimepicker_initialize = function(field) {
         $('.hasDateTimePicker').each(function() {
             $(this).removeClass('hasDateTimePicker');
-            $(this).datetimepicker({
-                pick12HourFormat: false,
-                sideBySide: true
-            });
+            $(this).datetimepicker($.extend({}, field.options));
         });
     };
 })(jQuery);
--- parts/fields/datetimepicker.php
+++ parts/fields/datetimepicker.php
@@ -1,5 +1,4 @@
 <input type="text" class="hasDateTimePicker"
-	   data-date-format="DD/MM/YYYY hh:mm"
 	   id="<?php echo piklist_form::get_field_id($field, $scope, $index, $prefix) ?>"
        name="<?php echo piklist_form::get_field_name($field, $scope, $index, $prefix) ?>"
        value="<?php echo is_array($value) ? esc_attr(end($value)) : esc_attr($value); ?>"
